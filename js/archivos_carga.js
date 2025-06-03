733d03ed46704e3ffa640443213197a1c75d5203
Dave1901-sudo <u22244804@utp.edu.pe>
2025-06-03 17:55:28 -0500

    Update archivos_carga.js

--- js/archivos_carga.js
+++ js/archivos_carga.js
@@ -4,13 +4,13 @@
 // en getID colocar el name del id_ que se encuentra en el index 
 
 //navbar
-fetch('../html/navbar.html')
+fetch('/html/navbar.html')
 .then(response => response.text())
 .then(data => {
     document.getElementById('navbar').innerHTML = data;
 });
 //footer
-fetch('../html/footer.html')
+fetch('/html/footer.html')
 .then(response => response.text())
 .then(data => {
     document.getElementById('footer').innerHTML = data;
